26e46e6540b9e2a3d132f089ba8a351042ce15a5
rotemhugi <rotemhugi@gmail.com>
2019-07-03 22:40:26 +0300

    Rotem1

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Register from './register/register.js';
 import Home from './home/home.js';
+import Passenger from './passenger/passenger.js';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
@@ -15,11 +16,12 @@ class App extends React.Component {
                 <Router>
                     <div>
                         <Link to="/">Home</Link>
-                        <Link to="/about">About</Link>
+                        <Link to="/passenger">Passenger</Link>
                         <Link to="/topics">Topics</Link>
                         <hr />
                         <Route exact path="/" component={Register} />
                         <Route path="/home" component={Home} />
+                        <Route path="/passenger" component={Passenger} />
                     </div>
                 </Router>
             </div>
