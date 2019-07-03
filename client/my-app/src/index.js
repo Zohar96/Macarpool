1f0f972e1284a7f8ee3fb0ba7442465cf4528d17
Zohar-UZ <zohar96@gmail.com>
2019-07-03 21:41:21 +0300

    add home page

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Register from './register/register.js';
+import Home from './home/home.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
     render() {
         return (
-            <div className="game">
-                <div className="game-board">
+            <div>
+                <div>
                     <Register />
                 </div>
+                <Router>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                        <Link to="/topics">Topics</Link>
+                        <hr />
+                        <Route exact path="/" component={Register} />
+                        <Route path="/home" component={Home} />
+                    </div>
+                </Router>
             </div>
+
         );
     }
 }
--- client/my-app/src/register/register.js
+++ client/my-app/src/register/register.js
@@ -1,52 +1,80 @@
-import React, { Component } from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
-import "./register.css";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 
-class Register extends Component {
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 200,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
+}));
+
+
+export default class Register extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { name: "", phone: "" };
 
-        this.state = {
-            phone: "",
-            name: ""
-        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event){
+        event.preventDefault();
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({ [name]: value });
     }
 
-    validateForm() {
+    validateForm = () => {
         return this.state.phone.length > 0 && this.state.name.length > 0;
     }
 
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-        });
-    }
-
     handleSubmit = event => {
-
+        alert("good");
+        //context.router.history.push('/home');
     }
 
     render() {
         return (
             <div className="Login">
-                <Form onSubmit={this.handleSubmit}>
-                    <FormGroup>
-                        <Label>איך קוראים לך? </Label>
-                        <Input name="name" type="text" id="name" placeholder="כינוי" value={this.state.name} onChange={this.handleChange}/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>טלפון</Label>
-                        <Input name="phone" type="text" id="phone" placeholder="טלפון" value={this.state.phone} onChange={this.handleChange}/>
-                    </FormGroup>
-                    <Button
-                        disabled={!this.validateForm()}
-                        type="submit"
-                        bsStyle="primary"
-                    >אפשר להיכנס פליזזזז?</Button>
-                </Form>
+                <FormControl>
+                    <TextField
+                        required
+                        id="outlined-name"
+                        label="כינוי"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <TextField
+                        required
+                        id="outlined-name"
+                        label="טלפון"
+                        name="phone"
+                        value={this.state.phone}
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <Button variant="contained" className={useStyles.button} disabled={!this.validateForm()} onClick={this.handleSubmit}>אפשר להיכנס פליזזזז?</Button>
+                </FormControl>
             </div>
         );
     }
 }
-
-export default Register;
